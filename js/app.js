13465debe7dc356e1ec804520082a9e634c657ea
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-11-26 16:35:43 +0100

    Created package.json file. Installed prerequisites.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by Mihail on 11/26/2016.
  */
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+React.render(
+	<h1>
+		<Logo /> Welcome to the app!
+	</h1>,
+	document.getElementById('app')
+);
--- js/source/components/Logo.js
+++ js/source/components/Logo.js
@@ -1,3 +1,13 @@
 /**
  * Created by Mihail on 11/26/2016.
  */
+'use strict';
+
+import React from 'react';
+
+class Logo extends React.Component {
+	render() {
+		return <div className="Logo" />;
+	}
+}
+export default Logo
