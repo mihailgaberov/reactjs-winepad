903c5bfaaebbc445150175ba096b49c621aae12e
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-11-27 11:58:35 +0100

    Added Excel table component.

--- src/app.js
+++ src/app.js
@@ -6,10 +6,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
+import Excel from './components/Excel';
+
+let headers = localStorage.getItem('headers');
+let data = localStorage.getItem('data');
+
+if (!headers) {
+	headers = ['Title', 'Year', 'Rating', 'Comments'];
+	data = [['Test Movie', '2015', '3', 'meh']];
+}
 
 ReactDOM.render(
-	<h1>
-		<Logo /> Welcome to the app!
-	</h1>,
+	<div>
+		<h1>
+			<Logo /> Welcome to the app!
+		</h1>
+		<Excel headers={headers} initialData={data} />
+	</div>,
 	document.getElementById('pad')
 );
